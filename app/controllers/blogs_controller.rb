e18338c67768c6df68c0172a6e868aea56a2ab26
EC2 Default User <ec2-user@ip-172-31-20-101.ap-northeast-1.compute.internal>
2023-07-28 05:27:12 +0000

    [Add]caravan02 blog

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,10 @@
+class BlogsController < ApplicationController
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_28_020113) do
+ActiveRecord::Schema.define(version: 2023_07_28_044850) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -38,6 +39,14 @@ ActiveRecord::Schema.define(version: 2023_07_28_020113) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "blog_title"
+    t.text "caption"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
